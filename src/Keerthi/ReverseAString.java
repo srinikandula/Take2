2d491f9398e39aab666ae88c6f720d13170f31ee
saikeerthip <keerthijkb@gmail.com>
2016-02-24 18:53:27 -0500

    String to upper case and reversea string

---
+++ src/Keerthi/ReverseAString.java
@@ -0,0 +1,25 @@
+        package keerthi;
+
+        public class ReverseAString {
+
+            public String reverseAString(String str){
+                String s1= str ;
+                String s2 = "";
+                char c=0 ;
+              // int l=  s1.length();
+                for(int  i=s1.length()-1;i>=0; i--){
+                   c =  s1.charAt(i);
+                   // System.out.println(c);
+                    s2 = s2 + c; //
+                }
+                // System.out.print(s2);
+                return s2;
+            }
+
+            public static void main(String[] args) {
+                ReverseAString r = new ReverseAString();
+                String result = r.reverseAString("keerthi");
+                System.out.println(result);
+            }
+
+        }
---
+++ src/Keerthi/ToUpperCase.java
@@ -0,0 +1,30 @@
+package keerthi;
+
+/**
+ * Created by CrazyNaveen on 2/24/16.
+ */
+public class ToUpperCase {
+
+    public void convertToUpperCase(){
+        String s1 = "hello world";
+        String s2 = "";
+        for(int i=0; i<=s1.length()-1;i++){
+            char c = s1.charAt(i);
+            System.out.print(c);
+
+            if(c >= 32 && c < 122 ){
+                c = (char)(c - 32);
+                s2 = s2 +c;
+            }
+        }
+        System.out.println(" ");
+        System.out.println(s2);
+    }
+    public static void main(String[] args) {
+        String s1= "hello world";
+        String s2 = s1.toUpperCase();
+        System.out.println(s2);
+        ToUpperCase t = new ToUpperCase();
+        t.convertToUpperCase();
+    }
+}
