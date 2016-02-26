8826baa0d395fd5007d2818ab7c662476612bdaf
gudedevendra <gudedevendra@gmail.com>
2016-02-26 18:56:21 -0500

    hello

---
+++ src/Devendra/ArithmaticExp.java
@@ -0,0 +1,19 @@
+package com.ExceptionEample;
+
+/**
+ * Created by devendra on 2/26/2016.
+ */
+public class ArithmaticExp {
+    public static void main(String[] args){
+       try {
+           int g = 1 / 0;
+           System.out.println(g);
+       }
+       catch(ArithmeticException e){
+           System.out.println("Division by 0  " +e.getMessage());
+          // e.printStackTrace();
+       }finally {
+           System.out.println("Thank you");
+       }
+    }
+}
---
+++ src/Devendra/OutBoundEx.java
@@ -0,0 +1,21 @@
+package com.ExceptionEample;
+
+/**
+ * Created by devendra on 2/26/2016.
+ */
+public class OutBoundEx {
+    public static void main(String[] args) {
+        try {
+            int [] d = {1,2,3,4,5,6};
+            d[6]=99;
+            System.out.println(d[6]);
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("this is array index out of bound Exception :" + e.getMessage());
+            //e.printStackTrace();
+        }finally {
+            System.out.println("Thank you");
+        }
+
+    }
+}
