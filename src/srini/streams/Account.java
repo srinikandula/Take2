4efd4db54992cac44f1749d4d9821db552ce4f20
srinikandula <kandula.srinivasarao@gmail.com>
2016-03-01 22:31:04 -0500

    more example with streams

---
+++ src/srini/streams/Account.java
@@ -0,0 +1,65 @@
+package srini.streams;
+
+import java.io.Serializable;
+
+/**
+ * Created by skandula on 3/1/16.
+ */
+public class Account implements Serializable{
+    private String firstName;
+    private String lastName;
+    private String accountType;
+    private long accNum;
+
+    public long getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(long ssn) {
+        this.ssn = ssn;
+    }
+
+    private transient long ssn;
+    private double balance;
+
+    public long getAccNum() {
+        return accNum;
+    }
+
+    public void setAccNum(long accNum) {
+        this.accNum = accNum;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+}
