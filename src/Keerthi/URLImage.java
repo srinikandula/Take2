168abc9c2156b97b60f885a49812fc2c99c6178e
saikeerthip <keerthijkb@gmail.com>
2016-03-03 15:31:53 -0500

    URL stream and URL image

---
+++ src/Keerthi/URLImage.java
@@ -0,0 +1,34 @@
+package keerthi;
+
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by CrazyNaveen on 3/2/16.
+ */
+public class URLImage {
+
+    public static void main(String[] args) {
+        URL img = null;
+        try {
+            img = new URL("http://www.funonline.in/wp-content/uploads/2011/12/Mr.-Bean-Cartoon-Photo-e1325063243535.jpg");
+            InputStream in = new BufferedInputStream(img.openStream());
+            OutputStream out = new BufferedOutputStream(new FileOutputStream("Mr.Bean.jpg"));
+
+            for (int i; (i = in.read()) != -1; ) {
+                out.write(i);
+            }
+            in.close();
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/Keerthi/URLStream.java
@@ -0,0 +1,37 @@
+    package keerthi;
+
+
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+    import java.net.MalformedURLException;
+    import java.net.URL;
+
+    /**
+     * Created by CrazyNaveen on 3/2/16.
+     */
+    public class URLStream {
+        public static void main(String[] args) {
+
+            URL urlStream = null;
+            try {
+                urlStream = new URL("http://angular.github.io/protractor/#/getting-started");
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(urlStream.openStream()))) {
+                    String inputLine;
+                    while ((inputLine = in.readLine()) != null) {
+                        if (inputLine.contains("AngularJS")) {
+                            System.out.println(inputLine);
+                        }
+                    }
+                    in.close();
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
