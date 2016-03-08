7544baf6242c31fae41e1e4f9b5de19c43b92cd7
gudedevendra <gudedevendra@gmail.com>
2016-03-07 20:13:28 -0500

    hello

---
+++ src/Devendra/Bank.java
@@ -0,0 +1,39 @@
+package Devendra;
+
+/**
+ * Created by devendra on 3/5/2016.
+ */
+public class Bank {
+    public int id;
+    public int account;
+
+
+    public void setaccount(int account) {
+        this.account = account;
+    }
+
+    public int getId(){
+
+        return id;
+    }
+    public void id(int id) {
+
+        this.id=id;
+    }
+    public int getAccount(){
+
+        return account;
+    }
+    public  void account(int account){
+
+        this.account=account;
+    }
+
+    public void setid(int id) {
+        this.id = id;
+    }
+
+
+
+    }
+
